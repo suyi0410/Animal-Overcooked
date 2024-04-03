5a24cd3d713f3315a6e547ad44d296a4ec2b2a1b
suyi0410 <suyi040410@gmail.com>
2024-04-03 15:28:11 +0800

    Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    
+
     [SerializeField] private float moveSpeed = 7f;
+    
+    
+    private bool isWalking;
+
     
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
@@ -23,11 +29,15 @@ public class Player : MonoBehaviour {
         }
         
         inputVector = inputVector.normalized;
-
+        
+        isWalking = (inputVector != Vector2.zero);
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+    public bool IsWalking(){
+        return isWalking;
     }
 }
