e59dfb2820899874005c8d029ba8bf16a0a50b14
suyi0410 <suyi040410@gmail.com>
2024-04-03 19:32:58 +0800

    Interact Action

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,31 @@ public class Player : MonoBehaviour {
     private bool isWalking;
 	private Vector3 lastInteractDir; 
 
+	private void Start(){
+		gameInput.OnInteractAction += GameInput_OnInteractAction;
+	}
+
+	private void GameInput_OnInteractAction(object sender, System.EventArgs e){
+	  
+		Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+		Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+	 
+		if (moveDir != Vector3.zero)
+	 {
+			lastInteractDir = moveDir;
+	 }
+	  
+		float interactDistance = 2f;
+		if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit,interactDistance, countersLayerMask))
+	 {
+			if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+			{
+				 // Has ClearCounter.cs
+				 clearCounter.Interact();
+			}
+	 }
+ }
+
 	private void Update(){
         HandleMovement();
         HandleInteractions();
@@ -35,8 +60,7 @@ public class Player : MonoBehaviour {
 		float interactDistance = 2f;
 		if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit,interactDistance,countersLayerMask)){
 			if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
-				 // 线击中的物体拥有ClearCounter.cs脚本
-				 clearCounter.Interact();
+
 			}
 		 } 
 	  
