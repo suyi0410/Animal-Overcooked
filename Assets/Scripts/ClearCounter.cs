43af9b2906ea04e8eeed82309d5c588d2fd113d8
suyi0410 <suyi040410@gmail.com>
2024-04-04 01:37:46 +0800

    Player Pick up, Drop Objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -10,8 +10,18 @@ public class ClearCounter : BaseCounter {
     
 
     public override void Interact(Player player){
-        
-    }
-    
-    
+        if (!HasKitchenObject()){
+            if (player.HasKitchenObject()){
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {
+
+            }
+        } else {
+                if (player.HasKitchenObject()){
+
+                } else {
+                    GetKitchenObject().SetKitchenObjectParent(player);
+                }
+        }
+     }  
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -12,9 +12,11 @@ public class ContainerCounter : BaseCounter {
     
 
     public override void Interact(Player player){
+        if (!player.HasKitchenObject()){
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab );
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
             OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
     }
 
 }
