7396b3b71fd18ff5d9e6e37b9b2a5ee03fc92c0f
suyi0410 <suyi040410@gmail.com>
2024-04-04 23:55:39 +0800

    Delivery Manager

---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour{
+    public static DeliveryManager Instance { get; private set; }
+    
+    [SerializeField] private RecipeListSO recipeListSO;
+    
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake(){
+        Instance = this;
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update(){
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0f){
+            spawnRecipeTimer += spawnRecipeTimerMax;
+
+            if (waitingRecipeSOList.Count < waitingRecipesMax){
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
+                Debug.Log(waitingRecipeSO.recipeName);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+            }
+        }
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject){
+        for (int i = 0; i < waitingRecipeSOList.Count; i++){
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+            if (waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count){
+                // 订单中的菜品与送上去的菜品由同样数量的物品组成
+                bool plateContentsMatchesRecipe = true;
+                foreach (KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList){
+                    // 遍历订单中的菜品所组成的物品
+                    bool ingredientFound = false;
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList()){
+                        // 遍历送上去的菜品所组成的物品
+                        if (recipeKitchenObjectSO == plateKitchenObjectSO){
+                            // 订单中的菜品与送上去的菜品由同样的物品组成
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+                    if (!ingredientFound){
+                        // 订单中的菜品与送上去的菜品由不同的物品组成
+                        plateContentsMatchesRecipe = false;
+                    }
+                }
+
+                if (plateContentsMatchesRecipe){
+                    Debug.Log("Player delivered the correct recipe!");
+                    waitingRecipeSOList.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+        // 遍历了所有订单，没有找到匹配的订单
+        Debug.Log("Player did not deliver a correct recipe!");
+    }
+}
