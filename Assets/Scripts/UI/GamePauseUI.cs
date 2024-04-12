a8650c73c09daf14b2de7a2387c1ebe3bbdf44aa
suyi0410 <suyi040410@gmail.com>
2024-04-12 16:17:16 +0800

    Options, Audio Levels

--- Assets/Scripts/UI/GamePauseUI.cs
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -7,13 +7,17 @@ using UnityEngine.UI;
 public class GamePauseUI : MonoBehaviour{
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button mainMenuButton;
-
+    [SerializeField] private Button optionsButton;
     private void Awake(){
         resumeButton.onClick.AddListener(() =>{
             KitchenGameManager.Instance.TogglePauseGame();
         });
         mainMenuButton.onClick.AddListener(() =>{
             Loader.Load(Loader.Scene.MainMenuScene);
+        });
+        optionsButton.onClick.AddListener(() =>
+        {
+            OptionsUI.Instance.Show();
         });
     }
 
