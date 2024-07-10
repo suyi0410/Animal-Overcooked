2ebbf825a8ee44b0d9fe942694fcde435a29682d
suyi0410 <suyi040410@gmail.com>
2024-07-11 02:35:07 +0800

    Install Netcode and basic Setup

--- Assets/Scripts/SelectCounterVisual.cs
+++ Assets/Scripts/SelectCounterVisual.cs
@@ -8,7 +8,7 @@ public class SelectCounterVisual : MonoBehaviour{
     [SerializeField] private GameObject[] visualGameObjectArray;
     
     private void Start(){
-        Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
+        //Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
     }
     
     private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e){
---
+++ Assets/Scripts/UI/Testing Netcode UI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestingNetcodeUI : MonoBehaviour
+{
+    [SerializeField]private Button startHostButton;
+    [SerializeField]private Button startClientButton;
+
+
+}
+
